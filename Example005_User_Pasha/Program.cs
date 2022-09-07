3790a08d570ad16a64571e41115d75ba73eddf27
PipBoy <pk7@ya.ru>
2022-09-07 17:28:08 +0700

    pasha_if_else

---
+++ Example005_User_Pasha/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "паша")
+{
+
+    Console.WriteLine("Ура, это же Паша!");
+}
+    else
+{
+
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
