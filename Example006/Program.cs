8bcd6996a7a181221ca50b60d326d8c63b7c5cfa
PipBoy <pk7@ya.ru>
2022-09-07 17:41:36 +0700

    Max_number_find

---
+++ Example006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 4;
+int c = 5;
+int d = 8;
+int e = 19;
+
+int max = a;
+
+if (a > max) max =  a;
+if (b > max) max =  b;
+if (c > max) max =  c;
+if (d > max) max =  d;
+if (e > max) max =  e;
+
+Console.Write("Мах = ");
+Console.WriteLine(max);
