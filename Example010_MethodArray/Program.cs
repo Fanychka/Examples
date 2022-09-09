666f64b4301e6f0b1003324a6490df15c3074485
PipBoy <pk7@ya.ru>
2022-09-10 02:21:11 +0700

    ArrayLength

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] array = {11,2,13,4,15,61,17,8};
+
+int n = array.Length;
+
+int find = 7;
+int index = 0;
+
+while (index < n) 
+{
+if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    }
+index = index++;
+}
+
